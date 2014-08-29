c975b896a3164b3b794f31d2a15eeda04f832746
Dion Bosschieter <dionbosschieter@gmail.com>
2014-08-29 16:37:50 +0200

    Replaced sqlserver junk with mysql

--- libs/DB.php
+++ libs/DB.php
@@ -7,38 +7,35 @@
  */
 class DB
 {
-    
-    private $_dbh;
-    protected static $instance;
-    
-    public static function getInstance() {
-        if(!isset(self::$instance)) {
-            self::$instance = new DB;
-        }
-        
-        return self::$instance;
-    }
-    
-    private function __construct() {
+  private $_dbh;
+  protected static $instance;
 
-        //init connection
-        $dsn = "sqlsrv:Server=".GGDConfig::DBSERVER;
-        $dsn.= ";Database=".GGDConfig::DBNAME.";";
+  public static function getInstance() {
+    if(!isset(self::$instance)) {
+      self::$instance = new DB;
+    }
 
-		try {
-		    $this->_dbh = new PDO($dsn,GGDConfig::DBUSER,GGDConfig::DBPASS);
-            
-            //PDO class
-            $this->_dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-            $this->_dbh->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
-		} catch (PDOException $ex) {
-		    echo $ex->getMessage();
-		}
-		
+    return self::$instance;
+  }
+
+  private function __construct()
+  {
+    //init connection
+    $dsn = "mysql:host=localhost;port=3306;dbname=dbname;";
+
+    try {
+      $this->_dbh = new PDO($dsn,"username","password");
+    } catch (PDOException $ex) {
+      echo $ex->getMessage();
     }
-    
-    public function getDB()
-    {
-        return $this->_dbh;
-    }
+
+    //PDO class
+    $this->_dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    $this->_dbh->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
+  }
+
+  public function getDB()
+  {
+    return $this->_dbh;
+  }
 }
